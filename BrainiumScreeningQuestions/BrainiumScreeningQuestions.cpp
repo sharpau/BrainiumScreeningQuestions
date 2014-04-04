83d836fe9f2e5fa20e5812d1b8c9a3cbb7df735b
Austin Sharp <austinsharp111@gmail.com>
2014-04-04 15:21:52 -0700

    akari problem done

--- BrainiumScreeningQuestions/BrainiumScreeningQuestions.cpp
+++ BrainiumScreeningQuestions/BrainiumScreeningQuestions.cpp
@@ -4,10 +4,12 @@
 #include "stdafx.h"
 
 void test_pente(void);
+void test_akari(void);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	test_pente();
+	test_akari();
 
 	return 0;
 }
--- BrainiumScreeningQuestions/akari.cpp
+++ BrainiumScreeningQuestions/akari.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 
 // direction vectors
-const std::pair<int, int> DIRECTIONS[8] = {
+const std::pair<int, int> DIRECTIONS[4] = {
 	std::make_pair<int, int>(1, 0),
 	std::make_pair<int, int>(0, 1),
 	std::make_pair<int, int>(-1, 0),
@@ -14,10 +14,38 @@ const int EMPTY = 0;
 
 typedef int puzzle[BOARD_SIZE][BOARD_SIZE];
 
-bool isIlluminated(puzzle p, std::pair<int, int> coord, std::vector<std::pair<int, int>> litCoords) {
+// returns true if coord is illuminated OR contains a light
+const bool isIlluminated(puzzle p, std::pair<int, int> coord, std::vector<std::pair<int, int>> litCoords) {
+	for (auto d : DIRECTIONS) {
+		int x = coord.first;
+		int y = coord.second;
 
+		// check each direction until we hit a number or a black square
+		while (p[x][y] == EMPTY && x >= 0 && y >= 0 && x < BOARD_SIZE && y < BOARD_SIZE) {
+			// does this square have a light?
+			if (std::find(litCoords.begin(), litCoords.end(), std::make_pair(x, y)) != litCoords.end()) {
+				return true;
+			}
+			x += d.first;
+			y += d.second;
+		}
+	}
+	return false;
 }
 
 void test_akari(void) {
+	puzzle test = { 0 };
+	test[0][3] = BLACK;
+	test[1][2] = 2;
+	test[2][1] = BLACK;
+	test[3][0] = 3;
+
+	std::vector<std::pair<int, int>> lights;
+	lights.push_back(std::make_pair(1, 1));
+
+	assert(isIlluminated(test, std::make_pair(0, 0), lights) == false);
+	assert(isIlluminated(test, std::make_pair(1, 0), lights) == true);
+	assert(isIlluminated(test, std::make_pair(1, 1), lights) == true);
+	assert(isIlluminated(test, std::make_pair(3, 3), lights) == false);
 
 }
